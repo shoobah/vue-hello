5b2fb642acd00586a5117039b1d705ccbb58ea89
shoobah <shoobah@gmail.com>
2017-11-13 22:12:29 +0100

    Some eslint and prettier stuff

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,29 +7,30 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Hej hej Vue'
+    name: 'HelloWorld',
+    data() {
+        return {
+            msg: 'Hej hej Vue'
+        };
     }
-  }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: bold;
+h1,
+h2 {
+    font-weight: bold;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
